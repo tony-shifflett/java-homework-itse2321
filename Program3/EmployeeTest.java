e34c5189596580a82a7bb00482b1b351c41c9903
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-06-09 16:11:06 -0500

    included assignment info in EmployeeTest

--- Program3/EmployeeTest.java
+++ Program3/EmployeeTest.java
@@ -1,9 +1,31 @@
+//********************************************************************
+//
+//  Author:        Charles Shifflett
+//
+//  Program #:     3
+//
+//  File Name:     EmployeeTest.java
+//
+//  Course:        ITSE 2321 Object-Oriented Programming - Java 
+//
+//  Due Date:      6/17/2022
+//
+//  Instructor:    Prof. Fred Kumi 
+//
+//  Chapter:       Chapter 2
+//
+//  Description:   Employee class and paired test implementation displaying
+//                 class methods and attributes
+//********************************************************************
 
 public class EmployeeTest{
     public static void main(String[] args){
         //create two employee objects with info provided in prompt
         Employee firstEmployee = new Employee ("John", "Doe", 2875.00);
         Employee secondEmployee = new Employee ("Jane", "Doe", 3150.75);
+
+        //call developer info as required in each assignment
+        developerInfo();
 
         //display employee info
         System.out.printf("%s %s earns $%,.2f per month%n", firstEmployee.displayFirstName(), 
@@ -25,5 +47,23 @@ public class EmployeeTest{
         System.out.printf("%s %s earns $%,.2f per month following a 20%% raise %n", secondEmployee.displayFirstName(), secondEmployee.displayLastName(),secondEmployee.displaySalary());
 
     }
-}
 
+    //***************************************************************
+    //
+    //  Method:       developerInfo
+    // 
+    //  Description:  The developer information method of the program
+    //
+    //  Parameters:   None
+    //
+    //  Returns:      N/A 
+    //
+    //**************************************************************
+    public static void developerInfo()
+    {
+       System.out.println("Name:    Charles Shifflett");
+       System.out.println("Course:  ITSE 2321 Object-Oriented Programming");
+       System.out.println("Program: Three \n");
+
+    } // End of the developerInfo method
+}
