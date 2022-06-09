d4e94d3bed5d1721739006adc8bbe00a7ba00269
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-06-08 22:11:15 -0500

    Employee class written with attributes and constructor, EmployeeTest instantiates one object of Employee class

---
+++ Program3/Employee.java
@@ -0,0 +1,15 @@
+
+
+public class Employee {
+    //class attributes
+    private String firstName;
+    private String lastName;
+    private double monthlySalary; 
+
+    //constructor
+    public Employee (String first, String last, double pay){
+        this.firstName = first; 
+        this.lastName = last;
+        this.monthlySalary = pay; 
+    }
+}
---
+++ Program3/EmployeeTest.java
@@ -0,0 +1,10 @@
+
+public class EmployeeTest{
+    public static void main(String[] args){
+
+        //instantiating an object of Employee class 
+        Employee testEmployee = new Employee("Tony", "Shifflett", 5000);
+
+    }
+}
+
