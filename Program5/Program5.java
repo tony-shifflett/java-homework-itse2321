151968d1dbd5433a13b17b503d2d95f822f1d298
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-06-25 13:31:29 -0500

    initial commit Program5 skeleton

---
+++ Program5/Program5.java
@@ -0,0 +1,59 @@
+//********************************************************************
+//
+//  Author:        Charles Shifflett
+//
+//  Program #:     Program 5
+//
+//  File Name:     Program5.java
+//
+//  Course:        ITSE 2321 Object-Oriented Programming - Java 
+//
+//  Due Date:      6/25/2022
+//
+//  Instructor:    Prof. Fred Kumi 
+//
+//  Chapter:       Chapter 6
+//
+//  Description:   Orientation Program
+//
+//********************************************************************
+
+public class Program5
+{
+    //***************************************************************
+    //
+    //  Method:       main
+    // 
+    //  Description:  The main method of the program
+    //
+    //  Parameters:   String array
+    //
+    //  Returns:      N/A 
+    //
+    //**************************************************************
+	public static void main(String[] args)
+    {
+	    developerInfo();
+
+
+	 
+    } // End of the main method
+	
+	//***************************************************************
+    //
+    //  Method:       developerInfo
+    // 
+    //  Description:  The developer information method of the program
+    //
+    //  Parameters:   None
+    //
+    //  Returns:      N/A 
+    //
+    //**************************************************************
+    public static void developerInfo()
+    {
+       System.out.println("Name:    Charles Shifflett");
+       System.out.println("Course:  ITSE 2321 Object-Oriented Programming");
+       System.out.println("Program: Five \n");
+
+    } // End of the developerInfo method
