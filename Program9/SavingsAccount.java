1bf40a06a1a16f3d80c01e50d6aa0e3ae49314e6
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-08-01 20:51:20 -0500

    constructor

--- Program9/SavingsAccount.java
+++ Program9/SavingsAccount.java
@@ -4,7 +4,30 @@ class SavingsAccount {
 
     private static double annualInterestRate;
     private double savingsBalance;
-    
+
+    SavingsAccount(double balance){
+        savingsBalance = balance;
+    }
+
+    private void calculateMonthlyInterest (){
+        double monthlyInterest = savingsBalance * annualInterestRate;
+    }
+
+    private void depositAmount(double deposit){
+        savingsBalance += deposit;
+    }
+
+    private void withdrawAmount(double withdrawal){
+        savingsBalance -= withdrawal;
+
+    }
+
+
+    private static void modifyInterestRate(float newRate){
+        annualInterestRate = newRate;
+    }
+
+
     public static void main (String [] args) {
         System.out.println("So far so good");
     }
